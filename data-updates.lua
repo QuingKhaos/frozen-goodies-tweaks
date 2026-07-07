adf6c1ac15dcaefed6818c936e176b0cfd00f956
Em Karisch <emii.khaos@gmail.com>
2026-07-07 22:37:04 +0200

    feat: unlock recipes with appropriate tech

--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,37 @@
+local khaoslib_recipe = require("__khaoslib__.recipe")
+local khaoslib_technology = require("__khaoslib__.technology")
+
 -- Move to bioprocessing tab if installed.
 if mods["bioprocessing-tab"] then
   data.raw["item-subgroup"]["fg-frozen"].group = "bioprocessing"
   data.raw["item-subgroup"]["fg-unfrozen"].group = "bioprocessing"
+end
+
+-- Make recipes unlock with the appropriate technology.
+local unlocks = {
+    ["fg-frozen-agricultural-science-pack"] = "agricultural-science-pack",
+    ["fg-frozen-bioflux"] = "bioflux",
+    ["fg-frozen-biter-egg"] = "captivity",
+    ["fg-frozen-fish"] = "fish-breeding",
+    ["fg-frozen-jelly"] = "jellynut",
+    ["fg-frozen-jellynut"] = "jellynut",
+    ["fg-frozen-nutrients"] = "agriculture",
+    ["fg-frozen-pentapod-egg"] = "biochamber",
+    ["fg-frozen-yumako"] = "yumako",
+    ["fg-frozen-yumako-mash"] = "yumako"
+}
+
+for goodie, tech in pairs(unlocks) do
+  khaoslib_recipe:load(goodie)
+    :set {enabled = false}
+    :commit()
+
+  khaoslib_recipe:load(goodie .. "-melt")
+    :set {enabled = false}
+    :commit()
+
+  khaoslib_technology:load(tech)
+    :add_unlock(goodie)
+    :add_unlock(goodie .. "-melt")
+    :commit()
 end
